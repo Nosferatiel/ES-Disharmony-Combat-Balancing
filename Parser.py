d3b93d673cd9628941393176081f4369fc4bb74b
Nosferatiel <nosferatiel@googlemail.com>
2013-12-30 15:26:53 +0100

    completed weapons readout, done some cosmetics, starting defense readout

--- Parser.py
+++ Parser.py
@@ -13,7 +13,8 @@ parser.add_option('--XmlDirectory', metavar='F', type='string', action='store', 
 argv = []
 
 #safeguard bools to kill the program before it does segmentation faults due to missing files
-WeaponsLoaded = False
+WeaponsLoaded  = False
+DefensesLoaded = False
 
 #top level structures for loading all necessary information
 WeaponData = list()
@@ -29,28 +30,34 @@ for files in os.listdir(options.XmlDirectory): #loading all xml files
 	counter = 0
 	for node in WeaponsList:
 	  name = node.attributes['Name'].value
-	  if(name.find('Fake') > 0): continue #skip all the fakes
+	  if(name.find('Fake')   >= 0): continue #skip all the fakes
+	  if(name.find('Hissho') >= 0): continue #skip all Hissho racial weapons
+	  if(name.find('Swarm')  >= 0): continue #skip all Craver racial weapons
 	  WeaponData.append(Weapon.struct()) #extend list of ROOT readable structs
 	  Sim = node.getElementsByTagName('Simulation')
 	  #enter all the values into the struct
-	  WeaponData[counter].Name = node.attributes['Name'].value
-	  WeaponData[counter].MinDmg = Sim[0].attributes['DamageMin'].value
-	  WeaponData[counter].MaxDmg = Sim[0].attributes['DamageMax'].value
+	  WeaponData[counter].Name     = node.attributes['Name'].value
+	  WeaponData[counter].MinDmg   = Sim[0].attributes['DamageMin'].value
+	  WeaponData[counter].MaxDmg   = Sim[0].attributes['DamageMax'].value
 	  WeaponData[counter].CritMult = Sim[0].attributes['CriticMultiplier'].value
-	  WeaponData[counter].CritMod = Sim[0].attributes['CriticChance'].value
-	  WeaponData[counter].Evasion = Sim[0].attributes['InterceptionEvasion'].value
-	  WeaponData[counter].Shots = Sim[0].attributes['NumberPerSalve'].value
-	  WeaponData[counter].HitTime = Sim[0].attributes['TurnBeforeReach'].value
-	  WeaponData[counter].Reload = Sim[0].attributes['TurnToReload'].value
+	  WeaponData[counter].CritMod  = Sim[0].attributes['CriticChance'].value
+	  WeaponData[counter].Evasion  = Sim[0].attributes['InterceptionEvasion'].value
+	  WeaponData[counter].Shots    = Sim[0].attributes['NumberPerSalve'].value
+	  WeaponData[counter].HitTime  = Sim[0].attributes['TurnBeforeReach'].value
+	  WeaponData[counter].Reload   = Sim[0].attributes['TurnToReload'].value
 	  WeaponData[counter].Accuracy = Sim[0].attributes['Accuracy'].value
 	  for subtype in Sim:
-	    Range = subtype.getElementsByTagName('RangeClass')
-	    Class = subtype.getElementsByTagName('WeaponClass')
-	    print subtype.childNodes[3].nodeName #how the hell do I get the valueof this???
-	    #WeaponData[counter].Range = Range[0].value
-	    #WeaponData[counter].Type = Weapon[0].value
+	    Range = subtype.childNodes[3].toxml() #childNodes[3] -> range
+	    Type = subtype.childNodes[1].toxml() #childNodes[1] -> Weapon type
+	    if(Range.find('Long')     >= 0): WeaponData[counter].Range = 1
+	    elif(Range.find('Medium') >= 0): WeaponData[counter].Range = 2
+	    elif(Range.find('Short')  >= 0): WeaponData[counter].Range = 3
+	    if(Type.find('Kinetic')   >= 0): WeaponData[counter].Type = 1
+	    elif(Type.find('Laser')   >= 0): WeaponData[counter].Type = 2
+	    elif(Type.find('Missile') >= 0): WeaponData[counter].Type = 3
 	  WeaponData[counter].Cost = node.attributes['Cost'].value
 	  WeaponData[counter].Tonnage = node.attributes['WeightFlat'].value	  
 	  counter += 1 #get ready for the next entry
-	print WeaponData[10].Accuracy
-	print len(WeaponData)
+  #part 2: defenses
+  if(files == 'DefenseModule.xml'):
+	DefensesLoaded = True
