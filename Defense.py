cfb0e19f9a7461a277e58253aa1b77ad7cb1c6a4
Nosferatiel <nosferatiel@googlemail.com>
2013-12-30 15:26:27 +0100

    Defense struct

---
+++ Defense.py
@@ -0,0 +1,10 @@
+class struct:
+	def __init__(self):
+		self.Name       = 'Empty'
+		self.Intercept  = 0
+		self.Deflection = 0
+		self.Absorption = 0
+		self.Defense    = 0		
+		self.Type       = 0 #1 kinetic, 2 laser, 3 missile
+		self.Cost       = 0
+		self.Tonnage    = 0
