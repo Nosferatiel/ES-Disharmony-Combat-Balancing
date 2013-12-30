84fbd824f2082d60426da3040fcb202a46e4ceb6
Nosferatiel <nosferatiel@googlemail.com>
2013-12-30 22:37:52 +0100

    bug fixing

--- Hull.py
+++ Hull.py
@@ -5,7 +5,7 @@ class struct:
 		self.Tonage			= 0
 		self.MaxSpecialSlotWeight	= 0
 		self.CommandPoint		= 0
-		self.Evasion			= 0
+		self.Evade			= 0
 		self.HullWeakness	 	= 0
 		self.EvasionDisorientation	= 0
 		self.Cost 			= 0
