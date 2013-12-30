5dd050be7ef38abb5b933a269f44502238838436
Nosferatiel <nosferatiel@googlemail.com>
2013-12-30 04:49:46 +0100

    struct for weapons data

---
+++ Weapon.py
@@ -0,0 +1,16 @@
+class struct:
+	def __init__(self):
+		self.Name = 'Empty'
+		self.MinDmg = 0
+		self.MaxDmg = 0
+		self.CritMult = 0
+		self.CritMod = 0
+		self.Evasion = 0
+		self.Shots = 0
+		self.HitTime = 0
+		self.Reload = 0
+		self.Accuracy = 0
+		self.Range = 0 #1 long range, 2 medium range, 3 melee
+		self.Type = 0 #1 kinetic, 2 laser, 3 missile
+		self.Cost = 0
+		self.Tonnage = 0
