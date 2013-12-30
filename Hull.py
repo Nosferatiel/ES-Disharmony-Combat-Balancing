b3cf23bdd9e79b5d2fa288e421a7e74b9048a6f7
BjornDeRijcke <adder@live.be>
2013-12-30 19:44:50 +0100

    New File

---
+++ Hull.py
@@ -0,0 +1,11 @@
+class struct:
+	def __init__(self):
+		self.Name 			= 'Empty'
+		self.MaxHealth 			= 0
+		self.Tonage			= 0
+		self.MaxSpecialSlotWeight	= 0
+		self.CommandPoint		= 0
+		self.Evasion			= 0
+		self.HullWeakness	 	= 0
+		self.EvasionDisorientation	= 0
+		self.Cost 			= 0
