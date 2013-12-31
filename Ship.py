2264b915d318d07d74feff62ad35e4b382c074ae
Nosferatiel <nosferatiel@googlemail.com>
2013-12-31 03:40:34 +0100

    ship strcut

---
+++ Ship.py
@@ -0,0 +1,9 @@
+class struct:
+	def __init__(self):
+		self.Health	   = 0
+		self.WeaponIndex   = 0
+		self.WeaponNumber  = 0
+		self.DefenseIndex  = 0
+		self.DefenseNumber = 0
+		self.HullWeakness  = 0
+		self.Maneuver      = 0
