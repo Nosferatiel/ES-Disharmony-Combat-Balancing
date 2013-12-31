08651b8e5796104cda394319478e6c69168229b6
Nosferatiel <nosferatiel@googlemail.com>
2013-12-31 03:41:47 +0100

    Added first launched ship configuration

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import Parser
+import Launch
 
 from optparse import OptionParser
 
@@ -22,7 +23,13 @@ for Hulls    in range(0,len(HullData)):
   print HullData[Hulls].Name
 
 #2 - generate fleets (TBD)
-
+#configure ship type and loadout here (later on subject to variation)
+Index_Hull    = 0
+Index_Weapon  = 0
+Index_Defense = 0
+WeaponFill   = 0.3 #describes the percentage of ship space filled by weaponry
+#generate ship struct with full loadout information
+ShipInventoryA = Launch.SimpleShip(HullData[Index_Hull].Tonnage, HullData[Index_Hull].MaxHealth, WeaponData[Index_Weapon].Type, WeaponData[Index_Weapon].Tonnage, DefenseData[Index_Defense].Type, DefenseData[Index_Defense].Tonnage, HullData[Index_Hull].Weakness, HullData[Index_Hull].Evade, WeaponFill)
 #3 - simulate fleet fight (TBD)
 
 #4 - fill histograms (TBD)
