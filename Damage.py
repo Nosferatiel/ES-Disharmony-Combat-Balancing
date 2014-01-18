5cb2195df316c286dd06b8b8838669566ed81d68
Nosferatiel <nosferatiel@googlemail.com>
2014-01-18 23:34:17 +0100

    fixed random number generator seed initialisation

--- Damage.py
+++ Damage.py
@@ -9,9 +9,9 @@ def Compute(minDmg, maxDmg, critChance, critMult, Defense, HullWeakness):
   Defense = float(Defense)
   HullWeakness = float(HullWeakness)
   #calculate the dmg output
-  DmgGenerator = TRandom3() #initialize random generator for damage
+  DmgGenerator = TRandom3(0) #initialize random generator for damage
   Dmg = DmgGenerator.Uniform(1)*(maxDmg-minDmg)+maxDmg #calculate bare damage
-  CritGenerator = TRandom3() #initialize random generator for crit
+  CritGenerator = TRandom3(0) #initialize random generator for crit
   if(CritGenerator.Uniform(1) <= critChance) or (critChance == 0): critMult = 1 #set critical multiplicator to 1, if there is no crit (either by design or chance)
   Dmg *= critMult #apply critical hit
   if(HullWeakness > 0): Dmg *= 1-1/(1+HullWeakness/Defense) #apply defense mitigation, if possible
