b413f7717ef780f572ea9b296648824ed4aac6de
Nosferatiel <nosferatiel@googlemail.com>
2014-01-18 18:58:07 +0100

    barebone ship damage

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import Parser
 import Launch
+import Damage
 
 from optparse import OptionParser
 
@@ -24,14 +25,20 @@ for Hulls    in range(0,len(HullData)):
 
 #2 - generate fleets (TBD)
 #configure ship type and loadout here (later on subject to variation)
-Index_Hull    = 0
-Index_Weapon  = 0
-Index_Defense = 0
+Index_Hull    = 2
+Index_Weapon  = 11
+Index_Defense = 2
 WeaponFill   = 0.3 #describes the percentage of ship space filled by weaponry
 #generate ship struct with full loadout information
-ShipInventoryA = Launch.SimpleShip(HullData[Index_Hull].Tonnage, HullData[Index_Hull].MaxHealth, WeaponData[Index_Weapon].Type, WeaponData[Index_Weapon].Tonnage, DefenseData[Index_Defense].Type, DefenseData[Index_Defense].Tonnage, HullData[Index_Hull].Weakness, HullData[Index_Hull].Evade, WeaponFill)
+ShipInventoryA = Launch.SimpleShip(HullData[Index_Hull].Tonnage, HullData[Index_Hull].MaxHealth, Index_Weapon, WeaponData[Index_Weapon].Tonnage, DefenseData[Index_Defense].Type, DefenseData[Index_Defense].Tonnage, HullData[Index_Hull].Weakness, HullData[Index_Hull].Evade, WeaponFill)
+ShipInventoryB = ShipInventoryA #clone, for now
+print str(WeaponData[ShipInventoryA.WeaponIndex].MaxDmg)
 #3 - simulate fleet fight (TBD)
-
+for phase in range(1,4): #phase loop from 1 to 3
+  for round in range (1,6): #round loop from 1 to 5
+    for salvoA in range (0, ShipInventoryA.WeaponNumber):
+	DmgA = Damage.Compute(WeaponData[ShipInventoryA.WeaponIndex].MinDmg, WeaponData[ShipInventoryA.WeaponIndex].MaxDmg, WeaponData[ShipInventoryA.WeaponIndex].CritMult, WeaponData[ShipInventoryA.WeaponIndex].CritMod, DefenseData[ShipInventoryB.DefenseIndex].Defense*ShipInventoryB.DefenseNumber, ShipInventoryB.HullWeakness)
+	print 'Damage ship A, ' + str(phase) + ', ' + str(round)+ ', ' + str(salvoA) + ': ' + str(DmgA)
 #4 - fill histograms (TBD)
 
 #5 - vary fleet parameters to fit target distribution (TBD)
